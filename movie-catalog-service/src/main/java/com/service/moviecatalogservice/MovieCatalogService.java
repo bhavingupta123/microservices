b12674e1f5067456011edac9b46ce2584f45d50a
Bhavin Gupta <bhavin-cs031@dsce.edu.in>
2022-01-04 18:38:15 +0530

    userRating API call done

--- movie-catalog-service/src/main/java/com/service/moviecatalogservice/MovieCatalogService.java
+++ movie-catalog-service/src/main/java/com/service/moviecatalogservice/MovieCatalogService.java
@@ -3,6 +3,8 @@ package com.service.moviecatalogservice;
 import com.service.moviecatalogservice.models.CatalogItem;
 import com.service.moviecatalogservice.models.Movie;
 import com.service.moviecatalogservice.models.Rating;
+import com.service.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +19,17 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        List<Rating> ratings = Arrays.asList(
-                new Rating("123",4),
-                new Rating("5678",5)
-        ); // hard coding the ratings for now
+      UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId , UserRating.class);
 
         // get details for each movie ID by calling movie info service
 
-        return ratings.stream().map(rating ->{
+        return ratings.getUserRating().stream().map(rating ->{
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class); // get all rated movies ID
             return new CatalogItem(movie.getName(),"test",rating.getRating());
         }).collect(Collectors.toList());
--- rating-data-service/src/main/java/com/service/ratingdataservice/RatingDataResource.java
+++ rating-data-service/src/main/java/com/service/ratingdataservice/RatingDataResource.java
@@ -1,9 +1,14 @@
 package com.service.ratingdataservice;
 
 import com.service.ratingdataservice.models.Rating;
+import com.service.ratingdataservice.models.UserRating;
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +17,17 @@ public class RatingDataResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId , 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings =  Arrays.asList(
+                new Rating("123",4),
+                new Rating("1235",4)
+        );
+
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
